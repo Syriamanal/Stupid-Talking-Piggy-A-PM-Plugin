51b8f87f6d05e6c40adf9d88ec9c123d3c0de142
Syriamanal <shadinachat123@gmail.com>
2015-05-07 07:49:42 -0700

    Update PIG.php

--- PIG/src/PIG.php
+++ PIG/src/PIG.php
@@ -1,13 +1,17 @@
 <?php
 namespace PIG;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 class Main extends PluginBase {
 
 public function onEnable(){
 $this->getLogger()->info(TextFormat::DARK_GREEN . "<Pig>" . "Pig Has Loaded!");
 }
-public function onSpawn(PlayerRespawnEvent $event){}
+public function onPlaerJoin(PlayerJoinEvent $event){}
+$p = $event->getPlayer();
+$p->sendMessage("<Pig> Welcome To Quantum Works MC!");
 
 }
